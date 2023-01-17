65b91a605fd9241dbb2a2c0eeaf7ceb945e54ebd
ray <raym>
2023-01-17 13:51:11 -0500

    backendProj

---
+++ node_http.js
@@ -0,0 +1,20 @@
+const http = require('http');
+//This imports the http module
+
+const server = http.createServer((req,res) =>{
+    if(req.url === '/'){
+        res.write("hello world");
+        res.write('this is our first server');
+        res.end();      
+    }
+    if(req.url === '/api/courses'){
+        res.write(JSON.stringify([1,2,3]))
+        res.write('This is a list of offerings');
+        res.write(' at BTHS');
+        res.end();
+    }
+});
+
+server.listen(3000);
+console.log("Listening on port 3000 ... ");
+
